829d2837acb8bebad992fc406b14364f43254028
Ana Luna <luna.ana92@gmail.com>
2020-07-19 00:32:44 -0700

    added password criteria and user propmts

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -13,17 +14,38 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Prompts
+// Password criteria
+var passwordLength = "8>=128";
+var numbers = "0123456789".split();
+var toUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split();
+var toLowerCase = "abcdefghijklmnopqrstuvwxyz".split();
+var specialChar = " !#$%&'()*+,-./:;<=>?@[^_`{|}~".split();
 
+
+// Promts for password criteria
 function generatePassword () {
-    var numberChar = "0123456789";
-    var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lowerChar = "abcdefghijklmnopqrstuvwxyz";
-    var specialChar = " !#$%&'()*+,-./:;<=>?@[^_`{|}~ ";
-    var allChar = "numberChar + upperChar + lowerChar + specialChar";
+  var passwordLength = prompt("Please enter the number of characters you want for you new password.  It must be more than 8 but less than 128.");
+  var numbers = confirm("Do you want numbers in your password?");
+  var toLowerCase = confirm("Do you want lowercases in your password?");
+  var toUpperCase = confirm("Do you want uppercases in your password?");
+  var specialChar = confirm("Do you want special characters in your password?");
+}
 
-} 
+// Input Validation
 
-alert ("Password Length")
+
+  
+  
+
+  
+
+
+  
+
+ 
+    
+
+
+
 
 
