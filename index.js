7a3c2d87c673657302c704213f3ec9ee4c317261
Ana Luna <luna.ana92@gmail.com>
2020-07-20 15:17:52 -0700

    adding new changes to index.js file

--- index.js
+++ index.js
@@ -26,12 +26,47 @@ var specialChar = " !#$%&'()*+,-./:;<=>?@[^_`{|}~".split();
 function generatePassword () {
   var passwordLength = prompt("Please enter the number of characters you want for you new password.  It must be more than 8 but less than 128.");
   var numbers = confirm("Do you want numbers in your password?");
-  var toLowerCase = confirm("Do you want lowercases in your password?");
-  var toUpperCase = confirm("Do you want uppercases in your password?");
+  var toLowerCase = confirm("Do you want lowercase in your password?");
+  var toUpperCase = confirm("Do you want uppercase in your password?");
   var specialChar = confirm("Do you want special characters in your password?");
 }
 
 // Input Validation
+function generatePassword () {
+  
+let password = [];
+if (numbers){
+  for (let i=0; i < 8; i++){password.push('randomChar')};
+}
+if (toLowerCase){
+  password = password.concat (toLowerCase);
+}
+if (toUpperCase){
+  password = password.contact (toUpperCase);
+}
+if (specialChar){
+  password = password.contact (specialChar);
+}
+
+function shuffle(array){
+  var password = array.passwordLength, temporaryValue, randompassword;
+  while (0 !==password){
+    randompassword = Math.floor(Math.random()* password);
+    password -=1;
+    temporaryValue = array [password];
+    array[password]= array [randompassword];
+    array[randompassword] = temporaryValue;
+
+  }
+  return array;
+
+}
+
+
+ //document.getElementById("password").innerHTML =
+// Math.floor(Math.random() * 101);   
+
+
 
 
   
